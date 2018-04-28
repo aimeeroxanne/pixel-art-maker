834cecd780b1f466df140461a71151641d02084a
Aimee Zawacki <aimee@tna.digital>
2018-04-27 19:14:32 -0600

    I built a simple pixel art maker

--- main.js
+++ main.js
@@ -3,22 +3,25 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-var canvas = document.getElementsByClassName('canvas')[0]
+  var canvas = document.getElementsByClassName('canvas')[0]
+
   for (let i = 0; i < 120; i++){
     let box = document.createElement('div')
     box.classList.add('box')
     canvas.appendChild(box)
   }
 
-let palette = document.getElementsByClassName('colors')[0]
+  let palette = document.getElementsByClassName('colors')[0]
 
-palette.addEventListener('click', function(event){
-  console.log(event.target)
-  let currentColor = event.target.className
-  console.log(currentColor)
-  canvas.addEventListener('click', function(event){
+  let swatch = document.getElementById('swatch')
+
+  palette.addEventListener('click', function(event){
+
+    let currentColor = event.target.className
+    swatch.className = currentColor
+
+    canvas.addEventListener('click', function(event){
     event.target.className = currentColor
+    })
   })
-})
-
 })
